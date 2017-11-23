f2be072b8e6454a7ecb28b9279072abbdee2a1d7
Matheus <pavinmatheus@gmail.com>
2017-11-23 20:45:26 -0200

    Iniciando o jogo

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,11 +2,15 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.GameInit;
 import com.mygdx.game.MyGdxGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);
+			config.title="tic tac toe";
+			config.width=600;
+			config.height=600;
+		new LwjglApplication(new GameInit(), config);
 	}
 }
