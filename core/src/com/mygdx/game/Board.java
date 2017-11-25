7cafff54c45d7b571cf9710b8bec2f8f12677d25
Matheus <pavinmatheus@gmail.com>
2017-11-25 18:39:40 -0200

    Ajustes na área

---
+++ core/src/com/mygdx/game/Board.java
@@ -0,0 +1,8 @@
+package com.mygdx.game;
+
+/**
+ * Created by root on 25/11/17.
+ */
+
+public class Board {
+}
---
+++ core/src/com/mygdx/game/Mark.java
@@ -0,0 +1,45 @@
+package com.mygdx.game;
+
+/**
+ * Created by Matheus on 25/11/17.
+ */
+
+public class mark {
+
+    int quadrant;
+    String mark;
+    int play;
+    int direction;
+
+    public int getQuadrant() {
+        return quadrant;
+    }
+
+    public void setQuadrant(int quadrant) {
+        this.quadrant = quadrant;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public int getPlay() {
+        return play;
+    }
+
+    public void setPlay(int play) {
+        this.play = play;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
+}
